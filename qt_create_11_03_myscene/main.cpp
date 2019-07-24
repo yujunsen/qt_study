954f9e3269da2c05b03dff09444bd358383ef34c
yujun <910387559@qq.com>
2019-07-24 10:42:42 +0800

    qtcreater 坐标系统

---
+++ qt_create_11_03_myscene/main.cpp
@@ -0,0 +1,29 @@
+#include <QApplication>
+#include <QGraphicsScene>
+
+#include "myitem.h"
+#include "myview.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QGraphicsScene scene;
+    scene.setSceneRect(0, 0, 400, 300);
+    MyItem *item = new MyItem;
+    scene.addItem(item);
+    item->setPos(10, 10);
+   // item->setZValue(1);
+    QGraphicsRectItem *rectItem = scene.addRect(QRect(0, 0, 100, 100), QPen(Qt::blue), QBrush(Qt::green));
+    item->setParentItem(rectItem);
+    rectItem->setRotation(45);
+    rectItem->setPos(20, 20);
+    MyView view;
+    view.setScene(&scene);
+    view.setForegroundBrush(QColor(255, 255,0, 100));
+    view.setBackgroundBrush(QPixmap("://background.png"));
+    view.resize(400, 300);
+    view.show();
+
+
+    return a.exec();
+}
