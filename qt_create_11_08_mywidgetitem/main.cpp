bafcefefc88c76bce3656c1c5f40fc9902913e5b
yujun <910387559@qq.com>
2019-07-24 16:55:36 +0800

    qtcreater 窗口部件、布局和内嵌部件

---
+++ qt_create_11_08_mywidgetitem/main.cpp
@@ -0,0 +1,34 @@
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QTextEdit>
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QGraphicsScene scene;
+    // 创建部件，并关联它们的信号和槽
+    QTextEdit *edit = new QTextEdit;
+    QPushButton *button = new QPushButton("clear");
+    QObject::connect(button, &QAbstractButton::clicked, edit, &QTextEdit::clear);
+    // 将部件添加到场景中
+    QGraphicsWidget *textEdit = scene.addWidget(edit);
+    QGraphicsWidget *pushButton = scene.addWidget(button);
+    // 将部件添加到布局管理器中
+    QGraphicsLinearLayout *layout = new QGraphicsLinearLayout;
+    layout->addItem(textEdit);
+    layout->addItem(pushButton);
+    // 创建图形部件，设置其为一个顶层窗口，然后在其上应用布局
+    QGraphicsWidget *form = new QGraphicsWidget;
+    form->setWindowFlags(Qt::Window);
+    form->setWindowTitle("Widget Title");
+    form->setLayout(layout);
+    // 将图形部件进行扭曲，然后添加到场景中
+    form->setTransform(QTransform().shear(2, -0.5), true);
+    scene.addItem(form);
+    QGraphicsView view(&scene);
+    view.show();
+
+    return a.exec();
+}
