fb1002a6fa4eb868926c81ec72d1af35a5691f00
yujun <910387559@qq.com>
2019-07-25 09:40:14 +0800

    qtcreater 实现属性动画

--- qt_create_11_09_myanimation/main.cpp
+++ qt_create_11_09_myanimation/main.cpp
@@ -7,12 +7,18 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QPushButton button("Animated Button");
     button.show();
-    QPropertyAnimation animation(&button, "geometry");
-    animation.setDuration(1000);
-//    animation.setStartValue(QRect(0, 0, 120, 30));
-//    animation.setEndValue(QRect(250, 250, 200, 60));
+    QPropertyAnimation animation(&button, "geometry");//获取geometry属性
+    animation.setDuration(10000);   //动画的持续时间 10s
+//    animation.setStartValue(QRect(0, 0, 120, 30));  //设置开始的geometry
+//    animation.setEndValue(QRect(250, 250, 200, 60));//设置结束的geometry
+
     animation.setKeyValueAt(0, QRect(0, 0, 120, 30));
-    animation.setKeyValueAt(0.8, QRect(250, 250, 200, 60));
-    animation.setKeyValueAt(1, QRect(0, 0, 120, 30));
+    animation.setKeyValueAt(0.8, QRect(250, 250, 200, 60));//8s 边城QRect
+    animation.setKeyValueAt(1, QRect(0, 0, 120, 30));   //2s 变回原样
+/*动画中间设值
+ * setKeyValueAt(qreal step, const QVariant &value)
+ * step 0.0-1.0 0.0开始位置 1.0结束位置
+*/
+    animation.start();
     return  app.exec();
 }
